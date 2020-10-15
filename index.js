0b5040c738f7cda9a999760f80e6413d90d80756
bwp91 <43026681+bwp91@users.noreply.github.com>
2020-10-15 10:08:49 +0100

    cap

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* jshint -W014, -W033, esversion: 9 */
 /* eslint-disable new-cap */
 'use strict'
-const GoveePlatform = require('./lib/govee-platform.js')
+const goveePlatform = require('./lib/govee-platform.js')
 module.exports = (hb) => {
-  hb.registerPlatform('homebridge-govee', 'Govee', GoveePlatform, true)
+  hb.registerPlatform('homebridge-govee', 'Govee', goveePlatform, true)
 }
