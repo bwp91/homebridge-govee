cac2d265b086a5a329cbaf75c0a75086f73e3154
bwp91 <43026681+bwp91@users.noreply.github.com>
2020-12-02 10:37:42 +0000

    file/dir structure

--- index.js
+++
@@ -1,5 +0,0 @@
-/* jshint -W014, -W033, esversion: 9 */
-/* eslint-disable new-cap */
-'use strict'
-const goveePlatform = require('./lib/govee-platform.js')
-module.exports = hb => hb.registerPlatform('homebridge-govee', 'Govee', goveePlatform)
